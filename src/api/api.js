adca940f3e140baae820cd595ecee8f365af943c
demolit_fanil@mail.ru <demolit_fanil@mail.ru>
2021-11-29 15:06:24 +0300

    link fix

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class Api {
 	getDaoInfo() {
 		return new Promise((resolve, reject) => {
-			axios.get('http://webapi.weezi.io/dao-info').then(({ data }) => resolve(data)).catch((e) => reject(e))
+			axios.get('https://webapi.weezi.io/dao-info').then(({ data }) => resolve(data)).catch((e) => reject(e))
 		})
 	}
 }
